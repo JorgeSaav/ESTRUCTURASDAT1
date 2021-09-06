bf35d392a837ac219d26446b04c3f3f8499dbc5f
Jorge Ramirez <Jorge2708Saavedra@hotmail.com>
2021-09-06 08:54:51 -0500

    Ejemplo

---
+++ Tareas/suma y resta 2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//Jorge Armando Ramirez Saavedra
+void resta(int op1, int op2, int *res);
+int suma(int op1, int op2);
+
+int main()
+{
+    int operando1;
+    int operando2;
+    int resultado;
+
+    printf("Operando1: ");
+    scanf("%d", &operando1);
+    printf("Operando2: ");
+    scanf("%d", &operando2);
+
+    resultado = suma(operando1, operando2);
+
+    printf("El resultado de la suma es: %d\n", resultado);
+
+    resta(operando1, operando2, &resultado);
+
+    printf("El resultado de la resta es %d\n", resultado);
+
+    return(0); 
+}
+
+int suma(int op1, int op2)
+{
+    return op1 + op2;
+}
+
+void resta(int op1, int op2, int *res)
+{
+    *res = op1 - op2;
+}
